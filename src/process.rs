3f77288734969f629586d92b1a0ebb3d59dfcfc8
Kunoi Sayami <i@leanhe.dev>
2022-04-12 01:21:23 +0800

    feat: Add a way to change prefix and bot name (#51)

--- src/process.rs
+++ src/process.rs
@@ -7,7 +7,7 @@ use crate::formatter::FormatContext;
 use crate::memory::ReplyBooking;
 use crate::parser::Parser;
 use crate::segments::{Segment, Segments};
-use crate::{EXPLAIN_COMMAND, EXPLAIN_COMMAND_EXTENDED};
+use crate::{COMMAND_PREFIX, EXPLAIN_COMMAND, EXPLAIN_COMMAND_EXTENDED};
 
 pub fn process(
     bot_user: &User,
@@ -23,9 +23,9 @@ pub fn process(
 
     let fmt_ctx = build_format_ctx(bot_user, booking, msg)?;
 
-    let parser = if text.starts_with(EXPLAIN_COMMAND_EXTENDED) {
+    let parser = if text.starts_with(EXPLAIN_COMMAND_EXTENDED.get().unwrap()) {
         Segments::build(text, entities)
-            .drain_head(EXPLAIN_COMMAND_EXTENDED.len() + 1)
+            .drain_head(EXPLAIN_COMMAND_EXTENDED.get().unwrap().len() + 1)
             .map(|segments| Parser::new(segments, true))
     } else if text.starts_with(EXPLAIN_COMMAND) {
         Segments::build(text, entities)
@@ -37,7 +37,7 @@ pub fn process(
                 Segments::build(text, entities)
                     .drain_head(1)
                     .map(|segments| Parser::new(segments, true))
-            } else if chr == '^' {
+            } else if chr == *COMMAND_PREFIX.get().unwrap() {
                 Segments::build(text, entities)
                     .drain_head(2)
                     .map(|segments| Parser::new(segments, true))
