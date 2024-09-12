5bf880f0755771c00d6407bee74910d4be51c5e2
LightQuantum <self@lightquantum.me>
2024-09-12 03:48:11 -0400

    fixup! refactor: update formatting, dependencies, and webhook handling

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@ use vergen_gix::{Emitter, GixBuilder};
 
 fn main() -> anyhow::Result<()> {
     // Generate the default 'cargo:' instruction output
-    Emitter::new().add_instructions(&GixBuilder::default().sha(true).build()?)?.emit()?;
+    Emitter::new()
+        .add_instructions(&GixBuilder::default().sha(true).build()?)?
+        .emit()?;
     Ok(())
 }
